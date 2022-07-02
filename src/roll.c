aaa4791fc8342123558251f2d7bb9eef516a4d1b
chrischriscris <18-10892@usb.ve>
2022-07-02 00:24:23 -0400

    roll funcional

--- src/roll.c
+++ src/roll.c
@@ -21,6 +21,8 @@ int rotate_file_content(char *path, void *m) {
     FILE *fp = fopen(path, "r+");
     if (!fp) return -1;
 
+    if (n == 0) return 0;
+
     /* Se obtiene el tamaño del archivo */
     fseek(fp, 0,  SEEK_END);
     size = ftell(fp);
@@ -38,25 +40,47 @@ int rotate_file_content(char *path, void *m) {
 
     /* Dependiendo del signo de n, se rota a la izq o der */
     if (n < 0) {
+        n = -n;
 
+        /* Se guardan los n caracteres al comienzo */
+        rewind(fp);
+        n_unread -= fread(buf2, 1, n, fp);
+
+        /* Va rotando bloques n posiciones desde el bloque
+        más a la izquierda al más a la derecha */
+        while (n_unread) {
+            int n_bytes = n_unread < BUFSIZ ? n_unread : BUFSIZ;
+
+            /* Toma un bloque */
+            fseek(fp, -n_unread, SEEK_END);
+            fread(buf1, 1, n_bytes, fp);
+
+            /* Lo coloca adelante n posiciones */
+            fseek(fp, -n - n_bytes, SEEK_CUR);
+            n_unread -= fwrite(buf1, 1, n_bytes, fp);
+        }
+
+        /* Escribe los n caracteres del comienzo al final */
+        fseek(fp, -n, SEEK_END);
+        fwrite(buf2, 1, n, fp);
     } else if (n > 0) {
         /* Se guardan los n caracteres al final */
         fseek(fp, size - n, SEEK_SET);
         n_unread -= fread(buf2, 1, n, fp);
 
-        /* Va rodando bloques hacia atrás */
-        while (n_unread >= BUFSIZ) {
-            fseek(fp, n_unread - BUFSIZ, SEEK_SET);
-            fread(buf1, 1, BUFSIZ, fp);
-            fseek(fp, n - BUFSIZ, SEEK_CUR);
-            n_unread -= fwrite(buf1, 1, BUFSIZ, fp);
-        }
+        /* Va rotando bloques n posiciones desde el bloque
+        más a la derecha al más a la izquierda */
+        while (n_unread) {
+            int n_bytes = n_unread < BUFSIZ ? n_unread : BUFSIZ;
 
-        /* Escribe el bloque de tamaño menor al buffer */
-        rewind(fp);
-        fread(buf1, 1, n_unread, fp);
-        fseek(fp, n, SEEK_SET);
-        n_unread -= fwrite(buf1, 1, n_unread, fp);
+            /* Toma un bloque */
+            fseek(fp, n_unread - n_bytes, SEEK_SET);
+            fread(buf1, 1, n_bytes, fp);
+
+            /* Lo coloca atrás n posiciones */
+            fseek(fp, n - n_bytes, SEEK_CUR);
+            n_unread -= fwrite(buf1, 1, n_bytes, fp);
+        }
 
         /* Escribe los n caracteres finales al comienzo */
         rewind(fp);
